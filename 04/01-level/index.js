6f54fce137150bbe7db543264f5efc4653606558
Ala Eddine <ala.official.acc@gmail.com>
2023-02-08 13:31:33 +0100

    :tada: Solve the day 4 exercices

---
+++ 04/01-level/index.js
@@ -0,0 +1,13 @@
+const input = prompt("Enter your age:");
+
+if (input >= 18) {
+  console.log("You are old enough to drive");
+} else {
+  console.log(`You are left with ${18 - input} years to drive`);
+}
+
+// Compare two numbers
+console.log(2 < 4 ? "Great" : "Less");
+
+
+// The other of exercices are easy
